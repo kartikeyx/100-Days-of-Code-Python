441eb7cbf64527c14b3fd4f62970f95b09781c96
Kartikey <75428247+kartikeyx@users.noreply.github.com>
2024-02-10 18:05:28 +0530

    Update number_guessing.py

--- Day 12/number_guessing.py
+++ Day 12/number_guessing.py
@@ -1,6 +1,10 @@
 import random
 from art import logo
-def guess():
+
+HARD_TURNS = 5
+EASY_TURNS = 10
+
+def gu():
   num = random.randint(1, 100)
   guess= int(input("Make a guess: "))
   if guess==num:
@@ -9,16 +13,19 @@ def guess():
     if guess>num:
       print("TOO HIGH")
 
+def difficulty_turns(difficulty):
+  if difficulty == "hard":
+    return HARD_TURNS
+  else:
+    return EASY_TURNS
 
 
 def game():
   print(logo)
   print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
   difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-  # turns = 0 
-  # if difficulty == "easy":
-  #   turns = 10
-    
-  # else:
-  #   turns = 5
+  difficulty_turns(difficulty)
+  
+
+
 
