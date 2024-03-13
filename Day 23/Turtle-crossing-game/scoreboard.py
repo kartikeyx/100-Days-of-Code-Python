9f355df877d079467e8104a18a9eaec35eb0a75a
Kartikey <75428247+kartikeyx@users.noreply.github.com>
2024-03-13 16:21:46 +0530

    :+1:

---
+++ Day 23/Turtle-crossing-game/scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+FONT = ("Courier", 16, "normal")
+ALIGNMENT = "center"
+
+
+class Scoreboard(Turtle):
+  def __init__(self):
+    super().__init__()
+    self.color("black")
+    self.penup()
+    self.goto(-230,240)
+    self.score = 1
+    self.hideturtle()
+    self.upd()
+    
+  def upd(self):
+    self.write(f"Level = {self.score}", align = ALIGNMENT , font = FONT)
+
+  def game_over(self):
+    self.goto(0,0)
+    self.write("GAME OVER", align = ALIGNMENT , font = FONT)
+
+  def inc(self):
+    self.score +=1
+    self.clear()
+    self.upd()
